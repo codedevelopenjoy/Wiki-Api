370729789363fcf6c776303f818ea28288f5ee62
Hardik Goel <hardikgoel19@gmail.com>
2020-04-23 12:13:54 +0530

    Done

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods = ['GET'])
 def home():
-    return render_template('index.html')
+    a1 = request.args.get('a')
+    b1 = request.args.get('b')
+    return render_template('index.html',a=str(a1),b=str(b1))
 
 if __name__ == "__main__":
     app.run(debug=True)
