e65eeda9c658ee37c24db081abae4a8df6a8c660
Hardik Goel <hardikgoel19@gmail.com>
2020-04-23 12:21:57 +0530

    FInal

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask,render_template,request
+import wikipedia
 
 app=Flask(__name__)
 
+KEY = 'MVwyYghfeMXlJOAP2LuYmPYuV'
+
 @app.route('/',methods = ['GET'])
 def home():
-    a1 = request.args.get('a')
-    b1 = request.args.get('b')
-    return render_template('index.html',a=str(a1),b=str(b1))
+    k = request.args.get('k')
+    l = request.args.get('l')
+    q = request.args.get('q')
+    if(k==KEY):
+        wikipedia.set_lang(str(l))
+        summary = wikipedia.summary(str(q))
+        return render_template('index.html',resp=str(summary))
+    else:
+        return render_template('index.html',resp='INVALID KEY')
 
 if __name__ == "__main__":
     app.run(debug=True)
