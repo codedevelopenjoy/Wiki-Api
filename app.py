d12d030c290a4d13b3d11d3e4e7a0a124451217a
Hardik Goel <hardikgoel19@gmail.com>
2020-04-23 12:30:20 +0530

    King

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import wikipedia
 
 app=Flask(__name__)
 
-KEY = 'MVwyYghfeMXlJOAP2LuYmPYuV'
+KEY = {'MVwyYghfeMXlJOAP2LuYmPYuV'}
 
 @app.route('/',methods = ['GET'])
 def home():
     k = request.args.get('k')
     l = request.args.get('l')
     q = request.args.get('q')
-    if(k==KEY):
+    if k in KEY:
         wikipedia.set_lang(str(l))
         summary = wikipedia.summary(str(q))
         return render_template('index.html',resp=str(summary))
